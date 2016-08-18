65c9335775688a15b344be4762ee7c75bd66bdb2
arturfelipe <arturfelipe.sousa@gmail.com>
2016-08-18 16:30:00 -0300

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='cities',
+    version='0.0.1',
+    description='Load data from cities and countries all over the world',
+    author='Artur Sousa',
+    author_email='arturfelipe.sousa@gmail.com',
+    url='https://github.com/arturfelipe/cities',
+    packages=find_packages(exclude=['demo']),
+    install_requires=[
+        'requests',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    long_description=read('README.md'),
+    license='MIT',
+    keywords='cities countries'
+)
