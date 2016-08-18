05510e87f1160dffce7d13977fc4d1f6373daa96
arturfelipe <arturfelipe.sousa@gmail.com>
2016-08-18 14:46:09 -0300

    Group confs within a dict

--- conf.py
+++ conf.py
@@ -3,26 +3,33 @@ import os
 
 SOURCES_DIR = 'sources'
 
-CITIES_SOURCE_PATH = os.path.join(SOURCES_DIR, 'cities.txt')
-COUNTRIES_SOURCE_PATH = os.path.join(SOURCES_DIR, 'countries.txt')
-
-CITIES_COLUMNS = (
-    'iso',
-    'iso3',
-    'iso_numeric',
-    'country',
-    'capital',
-    'area',
-    'population',
-    'continent',
-    'tld',
-    'currency_code',
-    'currency_name',
-    'phone',
-    'postal_code_format',
-    'postal_code_regex',
-    'languages',
-    'geonameid',
-    'neighbours',
-    'equivalent_fips_code',
-)
+SOURCES = {
+    'countries': {
+        'path': os.path.join(SOURCES_DIR, 'countries.txt'),
+        'url': 'http://download.geonames.org/export/dump/countryInfo.txt',
+        'collumns': (
+            'iso',
+            'iso3',
+            'iso_numeric',
+            'fips',
+            'country',
+            'capital',
+            'area',
+            'population',
+            'continent',
+            'tld',
+            'currency_code',
+            'currency_name',
+            'phone',
+            'postal_code_format',
+            'postal_code_regex',
+            'languages',
+            'geonameid',
+            'neighbours',
+            'equivalent_fips_code',
+        )
+    },
+    'cities': {
+        'path': os.path.join(SOURCES_DIR, 'cities.txt'),
+    }
+}
